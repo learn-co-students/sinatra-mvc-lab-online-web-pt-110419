4c91c1285367f7246a6043aa972d9b0f7a1d2af1
Thomas Wright <twwright@gmail.com>
2020-02-23 19:37:03 -0600

    Solves for two-letter words using "y" as a vowel
    Previous solution failed on line 21, where words like "by" and "my" is made entirely of consonant even though the "y" is being used as a vowel. New commit uses "word[2] != nil" to check if the word is three letters. A two-letter word made up of entirely consonants will either (a) already end or start in a vowel or (b) end with 'y'. Adding the check for a third letter ensures that all two-letter words are covered by the solution. :)

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -18,7 +18,7 @@ class PigLatinizer
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
     # word starts with 2 consonants
-    elsif consonant?(word[0]) && consonant?(word[1])
+    elsif word[2] != nil && consonant?(word[0]) && consonant?(word[1])
       word = word.slice(2..-1) + word.slice(0,2)
     # word starts with 1 consonant
     else
